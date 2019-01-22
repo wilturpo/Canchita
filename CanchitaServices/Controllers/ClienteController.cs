ada90ef5dddc22afbccb0063858ac1586c086e59
wilson <wins.el_s@hotmail.com>
2019-01-22 08:25:08 -0500

    primer commit
    CanchitaServices

---
+++ CanchitaServices/Controllers/ClienteController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CanchitaServices.Models.Repositories;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CanchitaServices.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClienteController : Controller
+    {
+        private IClienteRepository repositorio;
+        public ClienteController(IClienteRepository repo)
+        {
+            repositorio = repo;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public IQueryable<TCliente> Get()
+        {
+            return repositorio.Clientes;
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{ClienteId}")]
+        public TCliente Get(Guid ClienteID)
+        {
+
+            return repositorio.Clientes.Where(p => p.CliId == ClienteID).FirstOrDefault();
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public IActionResult Post([FromBody]TCliente cliente)
+        {
+            repositorio.SaveClient(cliente);
+            return Ok(true);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{ClienteId}")]
+        public IActionResult Put(Guid ClienteId, [FromBody]TCliente cliente)
+        {
+            cliente.CliId = ClienteId;
+            repositorio.SaveClient(cliente);
+            return Ok(true);
+        }
+
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{ClienteId}")]
+        public IActionResult Delete(Guid ClienteId)
+        {
+            repositorio.DeleteCliente(ClienteId);
+            return Ok(true);
+        }
+    }
+}
---
+++ CanchitaServices/Models/Repositories/IClienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using CanchitaServices.Models.Repositories;
+using Domain;
+using System;
+using System.Linq;
+
+namespace CanchitaServices.Models.Repositories
+{
+    public interface IClienteRepository
+    {
+        IQueryable<TCliente> Clientes { get; }
+        void SaveClient(TCliente cliente);
+        TCliente DeleteCliente(Guid ClienteID);
+    }
+}
